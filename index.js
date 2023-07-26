aa7a782d6f6376e211de0c4a861271d351f17233
zanthous <bcm023@gmail.com>
2023-07-26 04:34:52 -0400

    server

---
+++ index.js
@@ -0,0 +1,37 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8128;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  apiKey: process.env.MY_OPEN_AI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "日本語の家庭教師として働いています。日本語で対応し、間違いがあれば日本語で説明してください。",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
